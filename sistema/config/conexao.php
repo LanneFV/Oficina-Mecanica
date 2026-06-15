4e5cc6355ef2ec5866fdf95a00d300cb2fbd9ce4
LanneFV <elaneferreiraviana@gmail.com>
2026-06-15 08:56:09 -0300

    corrige conflitos html/js e conexao mysql

--- sistema/config/conexao.php
+++ sistema/config/conexao.php
@@ -1,2 +1,8 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "oficina");
+$conn = new mysqli("127.0.0.1", "root", "", "oficina");
+if ($conn->connect_error) {
+    http_response_code(500);
+    echo json_encode(['erro' => 'Erro de conexão: ' . $conn->connect_error]);
+    exit;
+}
+$conn->set_charset("utf8");
